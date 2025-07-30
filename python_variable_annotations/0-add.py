554e073889fe168d18f69692f2775020b0138a8f
LouisManchon <10578@holbertonstudents.com>
2025-07-30 22:38:06 +0200

    Add type-annotated function add to sum two floats

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Function add realised with variables annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function add return a + b
+    """
+    return a + b
