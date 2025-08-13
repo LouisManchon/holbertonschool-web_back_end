f0e25c0415afd1462f0562ce290a4ffe9d1a343b
LouisManchon <10578@holbertonstudents.com>
2025-08-13 14:23:33 +0200

    add docstrings

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+0-simple_helper_function.py
+Module containing the function index_range.
+"""
+
 def index_range(page: int, page_size: int) -> tuple:
     start = page * page_size - page_size
     end = page * page_size
