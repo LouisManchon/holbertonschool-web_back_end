5eccf737a1eaf55fb82ff5b442d65e78606522b7
LouisManchon <10578@holbertonstudents.com>
2025-09-05 03:55:10 +0200

    fix consol log

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,22 +1,52 @@
+// 5-http.js
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        return reject(new Error('Cannot load the database'));
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const fields = {};
+      let total = 0;
+
+      for (let i = 1; i < lines.length; i += 1) {
+        const [firstname, , , field] = lines[i].split(',');
+        if (field) {
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+          total += 1;
+        }
+      }
+
+      let output = `Number of students: ${total}\n`;
+      for (const [key, value] of Object.entries(fields)) {
+        output += `Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`;
+      }
+
+      resolve(output.trim());
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
   const file = process.argv[2];
   const { url } = req;
 
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+
   if (url === '/') {
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
     countStudents(file)
       .then((data) => res.end(`This is the list of our students\n${data}`))
-      .catch((err) => res.end(`This is the list of our students\n${err.message}`));
+      .catch(() => res.end('This is the list of our students\nCannot load the database'));
   }
 });
 
-app.listen(1245, 'localhost', () => {
-  console.log('Server running at http://localhost:1245/');
-});
+app.listen(1245);
 
 module.exports = app;
