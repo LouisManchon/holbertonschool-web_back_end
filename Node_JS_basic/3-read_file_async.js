008bac42e7a1cc4647dee74198efc770cc87c167
LouisManchon <10578@holbertonstudents.com>
2025-09-05 03:17:56 +0200

    fix function

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,41 +1,35 @@
 const fs = require('fs');
 
-async function countStudents(path) {
-  const myPromise = new Promise((resolve, reject) => {
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        return reject(new Error('Cannot load the database'));
       }
-      resolve(data);
+
+      const content = data.split('\n').filter((line) => line.trim() !== '');
+      const fields = {};
+      let total = 0;
+
+      for (let i = 1; i < content.length; i += 1) {
+        const [firstname, , , field] = content[i].split(',');
+
+        if (field) {
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
+          total += 1;
+        }
+      }
+
+      console.log(`Number of students: ${total}`);
+      for (const [key, value] of Object.entries(fields)) {
+        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      }
+
+      resolve();
     });
-  });
-
-  return myPromise.then((data) => {
-    const content = data.split('\n');
-    const fields = {};
-    let total = 0;
-
-    for (let i = 1; i < content.length; i += 1) {
-      const [firstname, , , field] = content[i].split(',');
-
-      if (!(field in fields) && field) {
-        fields[field] = [];
-      }
-
-      if (field) {
-        fields[field].push(firstname);
-        total += 1;
-      }
-    }
-
-    let result = `Number of students: ${total}`;
-
-    for (const [key] of Object.entries(fields)) {
-      result = result + `\nNumber of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`;
-    }
-
-    console.log(result);
-    return result;
   });
 }
 
